9621008f46afa01c9eb9a126939e485017fbb3d5
Nguyên Lê <52943968+nle-evizi@users.noreply.github.com>
2023-02-22 09:26:49 +0700

    Update 22/2/2023

---
+++ HGPT_APP/HGPT_APP.iOS/Renderer/ShellView.cs
@@ -0,0 +1,59 @@
+﻿using CoreGraphics;
+using Foundation;
+using HGPT_APP.iOS.Renderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ExportRenderer(typeof(Shell), typeof(ShellViewRenderer))]
+namespace HGPT_APP.iOS.Renderer
+{
+    public class ShellViewRenderer : ShellRenderer
+    {
+        protected override IShellPageRendererTracker CreatePageRendererTracker()
+        {
+            return new CustomShellPageRendererTracker(this);
+        }
+    }
+    public class CustomShellPageRendererTracker : ShellPageRendererTracker
+    {
+        public CustomShellPageRendererTracker(IShellContext context)
+            : base(context)
+        {
+
+        }
+
+        protected override void UpdateTitleView()
+        {
+            if (ViewController == null || ViewController.NavigationItem == null)
+                return;
+
+            var titleView = Shell.GetTitleView(Page);
+
+            if (titleView == null)
+            {
+                var view = ViewController.NavigationItem.TitleView;
+                ViewController.NavigationItem.TitleView = null;
+                view?.Dispose();
+            }
+            else
+            {
+                var view = new CustomTitleViewContainer(titleView);
+                ViewController.NavigationItem.TitleView = view;
+            }
+        }
+    }
+
+    public class CustomTitleViewContainer : UIContainerView
+    {
+        public CustomTitleViewContainer(View view) : base(view)
+        {
+            TranslatesAutoresizingMaskIntoConstraints = false;
+        }
+
+        public override CGSize IntrinsicContentSize => UILayoutFittingExpandedSize;
+    }
+}
---
+++ HGPT_APP/HGPT_APP/Interface/IShellView.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HGPT_APP.Interface
+{
+    internal interface IShellView
+    {
+    }
+}
